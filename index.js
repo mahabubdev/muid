f9c2ae6e575a6797bf23bba00c720de2aeb80a09
Md Mahabub Alam <mahabub2000bd@gmail.com>
2021-03-29 13:44:34 +0600

    muid dev v1

---
+++ index.js
@@ -0,0 +1,22 @@
+function mUID (baseNum = 16) {
+    /**--------------------------------------*
+     * baseNum is only ranged for 2 -> 32
+     * baseNum can accept only Integer value
+     *---------------------------------------*/
+
+    var timeNum = new Date().getTime();
+    var randNum = Math.floor(
+        Math.random(100, 999) * 10000
+    );
+
+    // prepare unique number
+    var uNum = timeNum * randNum || timeNum;
+
+    // generate the id key
+    var idKey = uNum.toString(baseNum);
+
+    // return the result
+    return idKey;
+}
+
+module.exports = mUID;
